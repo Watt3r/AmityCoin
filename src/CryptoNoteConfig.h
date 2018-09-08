32f1e2d02ba0096b23c41aae6dcd28b87725e437
Amity <42378180+Amity-Core@users.noreply.github.com>
2018-09-08 23:03:09 +0100

    True Genesis

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -64,7 +64,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 const char     CRYPTONOTE_NAME[]                             = "AmityCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "010f01ff000188f3b501029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210172e3227f886232f4deb585dbc69b663ed66d478ae350ddde992c2a81c66de079";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "010f01ff000188f3b501029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121015bd287fc30cbca9a3dc8fd9949d627c3dad7bef56774c3f5b0b5f72f2d05ef74";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
